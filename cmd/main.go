464c3de4b832a43cb72c3d25fd63cb268501ca93
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2022-06-05 14:29:47 +0800

    add package fyne.io/fyne for GUI, update go.sum and main.go.

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+)
 
+func main() {
+	App := app.New()
+
+	window := App.NewWindow("Terraria Editor")
+	window.Resize(fyne.Size{
+		Width:  600,
+		Height: 300,
+	})
+	window.CenterOnScreen()
+
+	window.ShowAndRun()
 }
